4fe8d2634d9203d137db06cf31d367e73396de63
Ranjbar.Arshia <Ranjbar.Arshia@Okala.Com>
2025-08-13 12:36:28 +0330

    reafactor: MainHero.tsx

--- src/components/MainHero.tsx
+++ src/components/MainHero.tsx
@@ -1,15 +1,12 @@
 import React from "react";
-import Foreign from "../assets/icons/Foreign.svg";
-import PlayOctagon from "../assets/icons/PlayOctagon.svg";
 
-import HeroImage from "../assets/hero.png";
 import HeroGIgili from "./Widgets/HeroGIgili";
+import { ForeignIcon, PlayOctagonIcon } from "../assets/icons";
 
 const MainHero = () => {
   return (
     <div className="hero">
       <div className="hero-content flex-col lg:flex-row lg:flex justify-between">
-        {/* Text Section: Takes 4/12 on large screens */}
         <div className="lg:w-4/12 w-full mt-4 lg:mt-0">
           <h1 className="text-5xl font-bold relative heigh leading-normal">
             <div className="absolute w-60 h-60 bg-[#FBD4C090] -top-24 -right-24 rounded-full blur-3xl -z-10"></div>
@@ -22,19 +19,18 @@ const MainHero = () => {
           </p>
           <div className="flex gap-4">
             <button className="btn btn-lg h-auto px-5 py-3.5 border-0 bg-primary text-white rounded-3xl font-bold gap-2.5 ">
-              <img src={Foreign} alt="Foreign Icon" />
+              <ForeignIcon />
               ازینجا شروع کن
             </button>
             <button className="btn btn-lg border-0 rounded-3xl font-bold gap-2.5 !bg-transparent">
-              <img src={PlayOctagon} alt="Foreign Icon" />
+              <PlayOctagonIcon />
               ازینجا شروع کن
             </button>
           </div>
         </div>
 
-        {/* Image Section: Takes 8/12 on large screens */}
         <div className="lg:w-6/12 w-full relative ">
-          <img src={HeroImage} alt="Hero Image" className="w-full " />
+          <img src="images/hero.png" alt="Hero Image" className="w-full " />
           <HeroGIgili />
           <HeroGIgili top={8} right={3} />
         </div>
